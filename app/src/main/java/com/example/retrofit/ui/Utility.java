196389977c2b8c76dd209045f39d549ddbeb5685
Ahmed <ahmmh778@gmail.com>
2020-06-17 16:00:19 +0200

    aplly changed

--- app/src/main/java/com/example/retrofit/ui/Utility.java
+++ app/src/main/java/com/example/retrofit/ui/Utility.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 
 public class Utility {
-    public static int calculateNoOfColumns(Context context, float columnWidthDp) { // For example columnWidthdp=180
+    // For example column Width dp=180
+    public static int calculateNoOfColumns(Context context, float columnWidthDp) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float screenWidthDp = displayMetrics.widthPixels / displayMetrics.density;
-        int noOfColumns = (int) (screenWidthDp / columnWidthDp + 0.5); // +0.5 for correct rounding to int.
+        int noOfColumns = (int) (screenWidthDp / columnWidthDp + 0.5);
+        // +0.5 for correct rounding to int.
         return noOfColumns;
     }
 
